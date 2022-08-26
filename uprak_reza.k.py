61c41b52e0c16433eb1c68dc6d356806573f8478
rezaarabgeming <111480249+rezaarabgeming@users.noreply.github.com>
2022-08-25 23:50:27 -0700

    Add files via upload

---
+++ uprak_reza.k.py
@@ -0,0 +1,31 @@
+print("1.balok\n")
+
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+tinggi = int(input("masukan nilai tinggi: "))
+
+luas_b = panjang * lebar * tinggi
+
+print("luas balok adalah: ",luas_b)
+
+print("2.limas\n")
+
+luas_alas = int(input("masukan nilai luas alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+
+luas_l = 1.3 * luas_alas * luas_alas  * tinggi
+
+print("luas limas adalah: ",luas_l)
+
+print("3.tabung\n")
+
+luas_alas = int(input("masukan nilai luas alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+
+luas_t = 22.7 * luas_alas * luas_alas * tinggi
+
+print("luas tabung adalah: ",luas_t)
+
+x = int(input("masukan nilai mata uang: "))
+y = x * 14000
+print(x,"dollar =",y,"rupiah")
